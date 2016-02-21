a05910a5a6b9084bbca5f58b500b108abfb2e403
Deepak Sharma <sadysnaat@gmail.com>
2016-02-21 12:50:54 +0530

    Completed Product related spec

--- lib/product.rb
+++ lib/product.rb
@@ -1,10 +1,12 @@
 class Product
-  attr_reader :title
+  attr_reader :title, :price, :stock
 
   @@products = []
 
   def initialize(options={})
     @title = options[:title]
+    @price = options[:price]
+    @stock = options[:stock]
     add_to_products
   end
 
@@ -12,12 +14,29 @@ class Product
     @@products
   end
 
+  def self.in_stock
+    @@products.select { |product| product.in_stock? }
+  end
+
+  def in_stock?
+    @stock > 0
+  end
+
+
+  def self.find_by_title(title)
+    @@products.each do |product|
+      if product.title == title
+        return product
+      end
+    end
+  end
+
   private
 
   def add_to_products
     all_products = @@products.map { |product| product.title }
     if all_products.include? @title
-      raise DuplicateProductError
+      raise DuplicateProductError, "'#{title}' already exists."
     end
     @@products << self
   end
