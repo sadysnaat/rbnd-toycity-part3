e7ef6ef5dd0976b18b86f7cd3863600ed868dcc5
Deepak Sharma <sadysnaat@gmail.com>
2016-02-21 11:25:14 +0530

    Added DuplicateProductError to errors

--- lib/product.rb
+++ lib/product.rb
@@ -1,11 +1,25 @@
 class Product
+  attr_reader :title
+
   @@products = []
 
   def initialize(options={})
-    @@products << self
+    @title = options[:title]
+    add_to_products
   end
 
   def self.all
     @@products
   end
+
+  private
+
+  def add_to_products
+    all_products = @@products.map { |product| product.title }
+    if all_products.include? @title
+      raise DuplicateProductError
+    end
+    @@products << self
+  end
+
 end
